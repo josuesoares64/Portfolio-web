40dd46c6341ac06f54245c68e4825132c2fd9dcb
josuesoares64 <seuemail@exemplo.com>
2025-07-07 20:37:31 -0300

    Corrigido build com Tailwind funcionando

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter } from 'next/font/google'
+import "./globals.css"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({
+  subsets: ['latin'],
+  variable: "--font-inter",
+})
 
 export const metadata: Metadata = {
   title: "Josué Soares | Desenvolvedor Web",
@@ -18,20 +13,18 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon-brasil.png",
   },
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-br">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="pt-br" className={inter.variable}>
+      <body className={`antialiased ${inter.className}`}>
         {children}
       </body>
     </html>
-  );
+  )
 }
